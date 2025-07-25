642013aa78c9931ed4292d92716e84b11429ffde
Cyborg_Ninja <cyborgninja2001@gmail.com>
2025-07-25 13:09:13 -0300

    ibm rom successfully recompiled into a c program

--- ibm.c
+++ ibm.c
@@ -1,15 +1,265 @@
 #include <stdio.h>
 #include <setjmp.h>
 #include <stdint.h>
+#include <string.h>
+#include <SDL2/SDL.h>
+
+#define WINDOW_WIDTH 64
+#define WINDOW_HEIGHT 32
+#define SCALE 10
+
+uint8_t font[80] = {
+0xF0, 0x90, 0x90, 0x90, 0xF0,
+0x20, 0x60, 0x20, 0x20, 0x70,
+0xF0, 0x10, 0xF0, 0x80, 0xF0,
+0xF0, 0x10, 0xF0, 0x10, 0xF0,
+0x90, 0x90, 0xF0, 0x10, 0x10,
+0xF0, 0x80, 0xF0, 0x10, 0xF0,
+0xF0, 0x80, 0xF0, 0x90, 0xF0,
+0xF0, 0x10, 0x20, 0x40, 0x40,
+0xF0, 0x90, 0xF0, 0x90, 0xF0,
+0xF0, 0x90, 0xF0, 0x10, 0xF0,
+0xF0, 0x90, 0xF0, 0x90, 0x90,
+0xE0, 0x90, 0xE0, 0x90, 0xE0,
+0xF0, 0x80, 0x80, 0x80, 0xF0,
+0xE0, 0x90, 0x90, 0x90, 0xE0,
+0xF0, 0x80, 0xF0, 0x80, 0xF0,
+0xF0, 0x80, 0xF0, 0x80, 0x80};
 
 uint8_t V[16];
 uint16_t I;
 uint8_t delay_timer;
 uint8_t sound_timer;
 uint8_t sp;
-uint16_t stack[16];
-uint8_t display[64][32];
+jmp_buf stack[16];
+uint8_t ram[4096];
+uint8_t display[WINDOW_WIDTH][WINDOW_HEIGHT];
+
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+void draw_screen() {
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	for (int i = 0; i < 32; i++) {
+		for (int j = 0; j < 64; j++) {
+			if (display[j][i]) {
+				SDL_Rect pixel = { j * SCALE, i * SCALE, SCALE, SCALE };
+				SDL_RenderFillRect(renderer, &pixel);
+			}
+		}
+	}
+	SDL_RenderPresent(renderer);
+}
+
+uint32_t last_timer_tick;
+void update_timers() {
+	if (SDL_GetTicks() - last_timer_tick >= 16) {
+		if (delay_timer > 0) delay_timer--;
+		if (sound_timer > 0) sound_timer--;
+		last_timer_tick = SDL_GetTicks();
+	}
+}
+#define CALL(x) if (setjmp(stack[sp])) { sp--; } else { sp++; goto x; }
+#define RET() longjmp(stack[sp - 1], sp);
+#define CLS() memset(display, 0, sizeof(display));
 
 int main(int argc, char* argv[]) {
+last_timer_tick = SDL_GetTicks();
+
+	memset(V, 0, sizeof(V));
+	I = 0;
+	delay_timer = 0;
+	sound_timer = 0;
+	sp = 0;
+	memset(ram, 0, sizeof(ram));
+	memcpy(ram + 0x050, font, sizeof(font));
+	memset(display, 0, sizeof(display));
+
+	FILE* f = fopen("ibm.ch8", "r");
+	fseek(f, 0, SEEK_END);
+	int rom_size = ftell(f);
+	rewind(f);
+	fread(ram + 0x200, 1, rom_size, f);
+	fclose(f);
+
+	SDL_Init(SDL_INIT_VIDEO);
+	window = SDL_CreateWindow("ibm logo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH * SCALE, WINDOW_HEIGHT * SCALE, 0);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+SDL_Event event;
+uint8_t x_cord;
+uint8_t y_cord;
+
+_200:
+	CLS()
+	update_timers();
+	SDL_Delay(16);
+_202:
+	I = 0x22A;
+	update_timers();
+	SDL_Delay(16);
+_204:
+	V[0x00] = 0x0C;
+	update_timers();
+	SDL_Delay(16);
+_206:
+	V[0x01] = 0x08;
+	update_timers();
+	SDL_Delay(16);
+_208:
+	x_cord = V[0x00] & 63;
+	y_cord = V[0x01] & 31;
+	V[0xF] = 0;
+	for (int row = 0; row < 0x0F; row++) {
+		uint8_t spryte = ram[I + row];
+		for (int col = 0; col < 8; col++) {
+			if((spryte & (0x80 >> col)) != 0) {
+				int dx = (x_cord + col) & 63;
+				int dy = (y_cord + row) & 31;
+				if (display[dx][dy]) V[0xF] = 1;
+				display[dx][dy] ^= 1;
+			}
+		}
+	}
+	draw_screen();
+	update_timers();
+	SDL_Delay(16);
+_20A:
+	V[0x00] += 0x09;
+	update_timers();
+	SDL_Delay(16);
+_20C:
+	I = 0x239;
+	update_timers();
+	SDL_Delay(16);
+_20E:
+	x_cord = V[0x00] & 63;
+	y_cord = V[0x01] & 31;
+	V[0xF] = 0;
+	for (int row = 0; row < 0x0F; row++) {
+		uint8_t spryte = ram[I + row];
+		for (int col = 0; col < 8; col++) {
+			if((spryte & (0x80 >> col)) != 0) {
+				int dx = (x_cord + col) & 63;
+				int dy = (y_cord + row) & 31;
+				if (display[dx][dy]) V[0xF] = 1;
+				display[dx][dy] ^= 1;
+			}
+		}
+	}
+	draw_screen();
+	update_timers();
+	SDL_Delay(16);
+_210:
+	I = 0x248;
+	update_timers();
+	SDL_Delay(16);
+_212:
+	V[0x00] += 0x08;
+	update_timers();
+	SDL_Delay(16);
+_214:
+	x_cord = V[0x00] & 63;
+	y_cord = V[0x01] & 31;
+	V[0xF] = 0;
+	for (int row = 0; row < 0x0F; row++) {
+		uint8_t spryte = ram[I + row];
+		for (int col = 0; col < 8; col++) {
+			if((spryte & (0x80 >> col)) != 0) {
+				int dx = (x_cord + col) & 63;
+				int dy = (y_cord + row) & 31;
+				if (display[dx][dy]) V[0xF] = 1;
+				display[dx][dy] ^= 1;
+			}
+		}
+	}
+	draw_screen();
+	update_timers();
+	SDL_Delay(16);
+_216:
+	V[0x00] += 0x04;
+	update_timers();
+	SDL_Delay(16);
+_218:
+	I = 0x257;
+	update_timers();
+	SDL_Delay(16);
+_21A:
+	x_cord = V[0x00] & 63;
+	y_cord = V[0x01] & 31;
+	V[0xF] = 0;
+	for (int row = 0; row < 0x0F; row++) {
+		uint8_t spryte = ram[I + row];
+		for (int col = 0; col < 8; col++) {
+			if((spryte & (0x80 >> col)) != 0) {
+				int dx = (x_cord + col) & 63;
+				int dy = (y_cord + row) & 31;
+				if (display[dx][dy]) V[0xF] = 1;
+				display[dx][dy] ^= 1;
+			}
+		}
+	}
+	draw_screen();
+	update_timers();
+	SDL_Delay(16);
+_21C:
+	V[0x00] += 0x08;
+	update_timers();
+	SDL_Delay(16);
+_21E:
+	I = 0x266;
+	update_timers();
+	SDL_Delay(16);
+_220:
+	x_cord = V[0x00] & 63;
+	y_cord = V[0x01] & 31;
+	V[0xF] = 0;
+	for (int row = 0; row < 0x0F; row++) {
+		uint8_t spryte = ram[I + row];
+		for (int col = 0; col < 8; col++) {
+			if((spryte & (0x80 >> col)) != 0) {
+				int dx = (x_cord + col) & 63;
+				int dy = (y_cord + row) & 31;
+				if (display[dx][dy]) V[0xF] = 1;
+				display[dx][dy] ^= 1;
+			}
+		}
+	}
+	draw_screen();
+	update_timers();
+	SDL_Delay(16);
+_222:
+	V[0x00] += 0x08;
+	update_timers();
+	SDL_Delay(16);
+_224:
+	I = 0x275;
+	update_timers();
+	SDL_Delay(16);
+_226:
+	x_cord = V[0x00] & 63;
+	y_cord = V[0x01] & 31;
+	V[0xF] = 0;
+	for (int row = 0; row < 0x0F; row++) {
+		uint8_t spryte = ram[I + row];
+		for (int col = 0; col < 8; col++) {
+			if((spryte & (0x80 >> col)) != 0) {
+				int dx = (x_cord + col) & 63;
+				int dy = (y_cord + row) & 31;
+				if (display[dx][dy]) V[0xF] = 1;
+				display[dx][dy] ^= 1;
+			}
+		}
+	}
+	draw_screen();
+	update_timers();
+	SDL_Delay(16);
+_228:
+	goto _228;
+	update_timers();
+	SDL_Delay(16);
+_22A:
 	return 0;
 }
--- src/recompilation.c
+++ src/recompilation.c
@@ -27,6 +27,12 @@ uint8_t font[80] = {
 
 uint8_t ram[RAM_SIZE];
 uint16_t pc = 0x200;
+uint16_t opcode;
+uint16_t nnn;
+uint8_t n;
+uint8_t x;
+uint8_t y;
+uint8_t kk;
 
 // int fprintf(FILE *stream, const char *format, ...);
 
@@ -48,7 +54,34 @@ int main(int argc, char* argv[]) {
     // libraries
     fprintf(f, "#include <stdio.h>\n");
     fprintf(f, "#include <setjmp.h>\n");
-    fprintf(f, "#include <stdint.h>\n\n");
+    fprintf(f, "#include <stdint.h>\n");
+    fprintf(f, "#include <string.h>\n");
+    fprintf(f, "#include <SDL2/SDL.h>\n\n");
+
+    // some macros
+    fprintf(f, "#define WINDOW_WIDTH 64\n");
+    fprintf(f, "#define WINDOW_HEIGHT 32\n");
+    fprintf(f, "#define SCALE 10\n\n");
+
+    // font
+    fprintf(f,
+        "uint8_t font[80] = {\n"
+        "0xF0, 0x90, 0x90, 0x90, 0xF0,\n"
+        "0x20, 0x60, 0x20, 0x20, 0x70,\n"
+        "0xF0, 0x10, 0xF0, 0x80, 0xF0,\n"
+        "0xF0, 0x10, 0xF0, 0x10, 0xF0,\n"
+        "0x90, 0x90, 0xF0, 0x10, 0x10,\n"
+        "0xF0, 0x80, 0xF0, 0x10, 0xF0,\n"
+        "0xF0, 0x80, 0xF0, 0x90, 0xF0,\n"
+        "0xF0, 0x10, 0x20, 0x40, 0x40,\n"
+        "0xF0, 0x90, 0xF0, 0x90, 0xF0,\n"
+        "0xF0, 0x90, 0xF0, 0x10, 0xF0,\n"
+        "0xF0, 0x90, 0xF0, 0x90, 0x90,\n"
+        "0xE0, 0x90, 0xE0, 0x90, 0xE0,\n"
+        "0xF0, 0x80, 0x80, 0x80, 0xF0,\n"
+        "0xE0, 0x90, 0x90, 0x90, 0xE0,\n"
+        "0xF0, 0x80, 0xF0, 0x80, 0xF0,\n"
+        "0xF0, 0x80, 0xF0, 0x80, 0x80};\n\n");
 
     // global variables
     fprintf(f, "uint8_t V[16];\n");
@@ -56,44 +89,144 @@ int main(int argc, char* argv[]) {
     fprintf(f, "uint8_t delay_timer;\n");
     fprintf(f, "uint8_t sound_timer;\n");
     fprintf(f, "uint8_t sp;\n");
-    fprintf(f, "uint16_t stack[16];\n");
-    fprintf(f, "uint8_t display[64][32];\n\n");
+    fprintf(f, "jmp_buf stack[16];\n"); // it's not necesary to initialize
+    fprintf(f, "uint8_t ram[4096];\n");
+    fprintf(f, "uint8_t display[WINDOW_WIDTH][WINDOW_HEIGHT];\n\n");
+
+    // sdl variables
+    fprintf(f, "SDL_Window* window = NULL;\n");
+    fprintf(f, "SDL_Renderer* renderer = NULL;\n\n");
+
+    // aux function
+    fprintf(f, "void draw_screen() {\n");
+    fprintf(f, "\tSDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);\n");
+    fprintf(f, "\tSDL_RenderClear(renderer);\n");
+    fprintf(f, "\tSDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);\n");
+    fprintf(f, "\tfor (int i = 0; i < 32; i++) {\n");
+    fprintf(f, "\t\tfor (int j = 0; j < 64; j++) {\n");
+    fprintf(f, "\t\t\tif (display[j][i]) {\n");
+    fprintf(f, "\t\t\t\tSDL_Rect pixel = { j * SCALE, i * SCALE, SCALE, SCALE };\n");
+    fprintf(f, "\t\t\t\tSDL_RenderFillRect(renderer, &pixel);\n");
+    fprintf(f, "\t\t\t}\n");
+    fprintf(f, "\t\t}\n");
+    fprintf(f, "\t}\n");
+    fprintf(f, "\tSDL_RenderPresent(renderer);\n");
+    fprintf(f, "}\n\n");
+
+    fprintf(f, "uint32_t last_timer_tick;\n");
+
+    fprintf(f, "void update_timers() {\n");
+    fprintf(f,
+        "\tif (SDL_GetTicks() - last_timer_tick >= 16) {\n"
+        "\t\tif (delay_timer > 0) delay_timer--;\n"
+        "\t\tif (sound_timer > 0) sound_timer--;\n"
+        "\t\tlast_timer_tick = SDL_GetTicks();\n"
+        "\t}\n"
+    );
+    fprintf(f, "}\n");
 
 
-    // first i should fprintf() all macros instructions to use later i think
+    // macros for isntructions
+    fprintf(f, "#define CALL(x) if (setjmp(stack[sp])) { sp--; } else { sp++; goto x; }\n");
+    fprintf(f, "#define RET() longjmp(stack[sp - 1], sp);\n");
+    fprintf(f, "#define CLS() memset(display, 0, sizeof(display));\n");
+    fprintf(f, "\n");
 
     fprintf(f, "int main(int argc, char* argv[]) {\n");
+    fprintf(f, "last_timer_tick = SDL_GetTicks();\n\n");
 
-    while (pc <= 0xFFFF) {
+    // initialize global variables
+    fprintf(f, "\tmemset(V, 0, sizeof(V));\n");
+    fprintf(f, "\tI = 0;\n");
+    fprintf(f, "\tdelay_timer = 0;\n");
+    fprintf(f, "\tsound_timer = 0;\n");
+    fprintf(f, "\tsp = 0;\n");
+    fprintf(f, "\tmemset(ram, 0, sizeof(ram));\n");
+    fprintf(f, "\tmemcpy(ram + 0x050, font, sizeof(font));\n");
+    fprintf(f, "\tmemset(display, 0, sizeof(display));\n\n");
+
+    // load the rom in ram
+    fprintf(f, "\tFILE* f = fopen(\"%s\", \"r\");\n", argv[1]);
+    fprintf(f, "\tfseek(f, 0, SEEK_END);\n");
+    fprintf(f, "\tint rom_size = ftell(f);\n");
+    fprintf(f, "\trewind(f);\n");
+    fprintf(f, "\tfread(ram + 0x200, 1, rom_size, f);\n");
+    fprintf(f, "\tfclose(f);\n\n");
+
+    // initialize SDL
+    fprintf(f, "\tSDL_Init(SDL_INIT_VIDEO);\n");
+    fprintf(f, "\twindow = SDL_CreateWindow(\"ibm logo\", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH * SCALE, WINDOW_HEIGHT * SCALE, 0);\n");
+    fprintf(f, "\trenderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);\n\n");
+    fprintf(f, "SDL_Event event;\n");
+    // aux variables for display
+    fprintf(f, "uint8_t x_cord;\n");
+    fprintf(f, "uint8_t y_cord;\n\n");
+
+
+    while (pc <= 0x230) { // TODO: fixme [only works for ibm.ch8]
         uint16_t opcode = (ram[pc] << 8) | ram[pc+1];
-        uint16_t nnn = opcode & 0xFFF;
-        uint8_t n = opcode & 0xF;
-        uint8_t x = (opcode & 0x0F00) >> 8;
-        uint8_t y = (opcode & 0x00F0) >> 4;
-        uint8_t kk = opcode & 0xFF;
+        nnn = opcode & 0xFFF;
+        n = opcode & 0xF;
+        x = (opcode & 0x0F00) >> 8;
+        y = (opcode & 0x00F0) >> 4;
+        kk = opcode & 0xFF;
+        fprintf(f, "_%03X:\n", pc);
         pc += 2;
 
-        switch(opcode & 0x8000) {
+        switch((opcode & 0xF000) >> 12) {
             case 0x0: {
-                switch(nnn) {
-                    case 0x00E0: {} break;
-                    case 0x00EE: {} break;
-                    default: { // SYS addr
-                        fprintf(f, "_%04X:\n", pc);
-                        fprintf(f, "\tgoto _%04X;\n", nnn);
+                switch(opcode) {
+                    case 0x00E0: { // CLS
+                        fprintf(f, "\tCLS()\n");
                     } break;
+                    case 0x00EE: { printf("0x00EE\n"); exit(-1); } break;
+                    default: { printf("0x0nnn\n"); exit(-1); } break;
                 }
             } break;
+            case 0x1: { // JP addr
+                fprintf(f, "\tgoto _%03X;\n", nnn);
+            } break;
+            case 0x6: { // LD Vx, byte
+                fprintf(f, "\tV[0x%02X] = 0x%02X;\n", x, kk);
+            } break;
+            case 0x7: { // ADD Vx, byte
+                fprintf(f, "\tV[0x%02X] += 0x%02X;\n", x, kk);
+            } break;
+            case 0xA: { // LD I, addr
+                fprintf(f, "\tI = 0x%03X;\n", nnn);
+            } break;
+            case 0xD: { // DRW Vx, Vy, nibble
+                fprintf(f, "\tx_cord = V[0x%02X] & 63;\n", x);
+                fprintf(f, "\ty_cord = V[0x%02X] & 31;\n", y);
+                fprintf(f, "\tV[0xF] = 0;\n");
+                fprintf(f, "\tfor (int row = 0; row < 0x%02X; row++) {\n", n);
+                fprintf(f, "\t\tuint8_t spryte = ram[I + row];\n");
+                fprintf(f, "\t\tfor (int col = 0; col < 8; col++) {\n");
+                fprintf(f, "\t\t\tif((spryte & (0x80 >> col)) != 0) {\n");
+                fprintf(f, "\t\t\t\tint dx = (x_cord + col) & 63;\n");
+                fprintf(f, "\t\t\t\tint dy = (y_cord + row) & 31;\n");
+                fprintf(f, "\t\t\t\tif (display[dx][dy]) V[0xF] = 1;\n");
+                fprintf(f, "\t\t\t\tdisplay[dx][dy] ^= 1;\n");
+                fprintf(f, "\t\t\t}\n");
+                fprintf(f, "\t\t}\n");
+                fprintf(f, "\t}\n");
+                fprintf(f, "\tdraw_screen();\n");
+            } break;
             default: {
-                printf("[ERROR]: opcode not yet mapped 0x%01X\n", (opcode & 0x8000) >> 12);
+                printf("[ERROR]: opcode not yet mapped 0x%01Xxx%01X\n", (opcode & 0xF000) >> 12, n);
                 fprintf(f, "\treturn 0;\n");
                 fprintf(f, "}\n");
                 fclose(f);
                 exit(-1);
             }
         }
+        fprintf(f, "\tupdate_timers();\n");
+        fprintf(f, "\tSDL_Delay(16);\n");
     }
 
+    fprintf(f, "SDL_DestroyRenderer(renderer);\n");
+    fprintf(f, "SDL_DestroyWindow(window);\n");
+    fprintf(f, "SDL_Quit();\n");
     fprintf(f, "\treturn 0;\n");
     fprintf(f, "}\n");
     fclose(f);
